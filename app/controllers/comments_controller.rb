200b9e17cfee46d4099c9578e20c1579530367de
slye20 <samuellim2011@gmail.com>
2024-01-02 21:51:57 +0800

    Functionality to update and delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,12 +12,30 @@ class CommentsController < ApplicationController
 			render json: { thread: @comment, }, status: :created
 		else 
 			render json: { error: @comment.errors.full_messages }, status: :not_acceptable
+		end
 	end
-end
+
+	def update 
+    @comment = Comment.find(params[:id])
+    if @comment.update(content: comment_params[:content])
+      render json: { thread: @comment, }, status: :ok
+    else
+      render json: { error: @comment.errors.full_messages }, status: :not_acceptable
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.delete
+      render status: :ok
+    else
+      render json: { error: @comment.errors.full_messages }, status: :not_acceptable
+    end
+  end
+
 	private
 
 	def comment_params
-			params.require(:comment).permit(:content, :id)
+			params.require(:comment).permit(:content, :id, :author, :created_at)
 	end
-
 end
