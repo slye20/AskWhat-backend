08014b6d6c28850424d19c22be53fe78d09ec299
slye20 <samuellim2011@gmail.com>
2024-01-15 09:47:12 +0800

    Edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get '/me', to: 'users#show'
   post '/signup', to: 'users#create'
   get '/search', to: 'search#show'
-  resources :forum_threads, only: %i[create show index]
-  resources :comments, only: %i[create show index destroy]
+  resources :forum_threads, only: %i[create show index update destroy]
+  resources :comments, only: %i[create index update destroy]
   resources :categories, only: %i[show index]
   resources :users, only: %i[create show index]
 end
