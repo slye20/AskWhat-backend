5a504a7308e80bf5ed82859f9a5dbccf5eb8b5da
slye20 <samuellim2011@gmail.com>
2024-01-17 10:58:16 +0800

    Edited CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'
+    origins 'http://localhost:3000', 'http://localhost:3001', 'https://askwhat-backend-195dc318e9d7.herokuapp.com/'
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true
   end
 end
