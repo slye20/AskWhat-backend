16e759b1dd7b4922ab875bf496b780a5c2ab4de6
slye20 <samuellim2011@gmail.com>
2024-01-19 10:37:52 +0800

    Changed url of frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:3001', 'https://askwhat-1b26934c0d62.herokuapp.com/'
+    origins 'http://localhost:3000', 'http://localhost:3001', 'https://askwhat-1b26934c0d62.herokuapp.com'
 
     resource '*',
              headers: :any,
