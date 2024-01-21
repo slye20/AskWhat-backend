faaed40a36a648cef7555b7e17939a7edc2192ef
slye20 <samuellim2011@gmail.com>
2024-01-22 01:20:28 +0800

    Final changes

--- app/models/forum_thread.rb
+++ app/models/forum_thread.rb
@@ -1,8 +1,8 @@
 class ForumThread < ApplicationRecord
-    belongs_to :user
-    has_and_belongs_to_many :category
-    has_many :comments, dependent: :destroy
-    
-    validates :title, presence: true, uniqueness: true, length: 10..100, allow_blank: false
-    validates :user_id, presence: true
+  belongs_to :user
+  has_and_belongs_to_many :category
+  has_many :comments, dependent: :destroy
+
+  validates :title, presence: true, uniqueness: true, length: 10..100, allow_blank: false
+  validates :user_id, presence: true
 end
